1f0b1ae86b83f8b1762ed260be6aaf0b7bfd5920
janson <gandancing@gmail.com>
2016-04-12 14:08:14 +0800

    add project files

---
+++ example/src/main/java/com/myapp/bootstrap/ExampleServerBootstrapBean.java
@@ -0,0 +1,12 @@
+package com.myapp.bootstrap;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class ExampleServerBootstrapBean implements ApplicationListener<ApplicationEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+    }
+
+}
